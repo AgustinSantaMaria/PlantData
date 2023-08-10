2b624fb9673ca29fbb177488b16713d61a8d4b52
AgustinSantaMaria <102297458+AgustinSantaMaria@users.noreply.github.com>
2023-08-10 18:44:07 -0300

    Creacion de FamilyController, constructor y metodo GetAll()

---
+++ PlantData.API/Controllers/FamilyController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.SqlServer.Query.Internal;
+using PlantData.API.Data;
+using PlantData.API.Models.Domian;
+
+namespace PlantData.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FamilyController : ControllerBase
+    {
+        private readonly PlantDataDbContext plantDataDbContext;
+
+        public FamilyController(PlantDataDbContext plantDataDbContext)
+        {
+            this.plantDataDbContext = plantDataDbContext;
+        }
+
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var families = plantDataDbContext.Families.ToList();
+            return Ok(families);
+        }
+
+        [HttpGet]
+        [Route("{id:Guid}")]
+        public IActionResult GetById([FromRoute] Guid id)
+        {
+
+        }
+    }
+}
